af0e73e5b3e15511eeae59093b15964200d97434
Shudhanshusoni <97749507+Shudhanshusoni@users.noreply.github.com>
2022-12-01 15:18:42 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer=()=>{
+    return (
+        <div className="footer">
+        <h1 >E-Commerce Dashboard</h1>
+        <p>By Shudhanshu soni</p>
+        </div>
+    )
+}
+export default Footer;
